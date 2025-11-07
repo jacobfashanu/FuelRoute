dcf6090b402fccd835c3649b957933bc4ffee04b
Michael Lau <lau0@sheridancollege.ca>
2025-11-07 15:00:05 -0500

    Added user class file

---
+++ FuelRoute.Core/Models/User.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace FuelRoute.Core.Models
+{
+    public class User
+    {
+        [Required, StringLength(35)]
+        public string FirstName { get; set; } = string.Empty;
+        [Required, StringLength(35)]
+        public string LastName { get; set; } = string.Empty;
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required, RegularExpression(@"^\d{3}-\d{3}-\d{4}$", ErrorMessage = "Use format: 905-123-4567")]
+        public string Phone { get; set; } = string.Empty;
+        [Required]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters long.")]
+        [DataType(DataType.Password)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).+$",
+        ErrorMessage = "Password must include upper, lower, number, and special character.")]
+        public string Password { get; set; } = string.Empty;
+        // Add hashing and salting later
+    }
+}
